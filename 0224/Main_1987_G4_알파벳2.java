3720139368e34c15fdb2b76c183d75306efb28b7
6v6DS <dasul100@ajou.ac.kr>
2025-02-24 18:49:11 +0900

    feat:2025.02.24

--- 0224/Main_1987_G4_알파벳2.java
+++ 0224/Main_1987_G4_알파벳2.java
@@ -10,8 +10,7 @@ import java.util.StringTokenizer;
  *   준비 단계
  *   1.  1,1 ~ R, C까지 좌표지만 좌표로 딱히 다른 계산을 하지 않고 최대 이동 거리만 나오므로 
  *   0,0 ~ R-1, C-1 좌표를 사용한다. 
- *   2. 방문 배열을 [R][C]만큼 할 필요 없이 [26]개의 일차원 배열을 사용한다. 
- *   ==> 이미 이동한 알파벳을 또 이동 할 수 없으므로 알파벳에 대한 방문 처리만 하면 된다. 
+ *   2. 방문 체크할 [26]개의 일차원 배열 대신 bitmask 이용.
  *   3. 방문한 알파벳을 체크하기 위해 map은 char배열로 해서 -'A' 한 위치에서 방문 체크를 해도 되지만 
  *      편히 쓰기 위해 int[][]map을 선언한다.   
  *   4. 방문시 이미 방문한 알파벳이라면 뒤로 돌아가서 다른 길을 탐색해야 하므로 DFS로 탐색한다.
@@ -47,7 +46,7 @@ public class Main_1987_G4_알파벳2 {
 
     static int R, C, max;
     static int[][] map;
-    static int[][] visited;
+    static int[][] visited;     // [r][c]에 여태까지 방문한 알파벳을 bitmask로 기록
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -67,7 +66,7 @@ public class Main_1987_G4_알파벳2 {
                 map[i][j] = str.charAt(j) - 'A';
             }
         }
-        dfs(0, 0, 1 << map[0][0], 1);
+        dfs(0, 0, 1 << map[0][0], 1);       //여기부터가 방문기록 시작.
         System.out.println(max);
     }
 
@@ -75,14 +74,22 @@ public class Main_1987_G4_알파벳2 {
     	max = Math.max(max, cnt);
         if (max == 26)
             return;
-        visited[r][c] = bit;
+        //시작 좌표에서 현재 좌표까지 방문한 알파벳을 메모한다.
+        visited[r][c] = bit;        // 이게 하나 더 추가됨. 방문체크 배열에 기록.
 
         for (int i = 0; i < 4; i++){
             int nr = r + dr[i];
             int nc = c + dc[i];
+            //경계 밖이면 더 이상 진행하지 않음
             if(nr<0 || nr>=R || nc<0|| nc>=C) continue;
+
+                //방문 체크(현재 좌표의 알파벳이 있는지 체크)
             if ((bit & 1 << map[nr][nc]) == 0 &&
-                    (bit | 1 << map[nr][nc]) != visited[nr][nc]) {
+                    // 개수 상관 없이.. 다르기만 하면 가보는 것.
+                    // count가 중요한 것이 아닌, 다른 것을 체크하는 것이기 때문에.
+                    // map[nr][nc]위치의 알파벳을 bit에 추가한 후 이전 기록과 같은지 체크
+                    (bit | 1 << map[nr][nc]) != visited[nr][nc]) {  // 같으면 안가고 ==> 탐색 해봤자 같은 경우의 수이므로
+                //이전 기록과 같지 않은 알파벳 조합이라면 dfs로 탐색 해본다.
                 dfs(nr, nc, bit | 1 << map[nr][nc], cnt + 1);
             }
         }
