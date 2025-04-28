ccfdac9bd1016a1ea5d09fe204b65bde9c0be352
6v6DS <dasul100@ajou.ac.kr>
2025-04-28 17:47:35 +0900

    feat:2025.04.28

--- 0428/01_auth/02_next_auth/src/components/books/BookFetch.tsx
+++ 0428/01_auth/02_next_auth/src/components/books/BookFetch.tsx
@@ -1,12 +1,11 @@
 import { Book } from "@/types/book";
-import { localAxios } from "@/utils/http-commons";
+import axios from "@/utils/http-commons";
 import styles from "./books.module.scss";
 export default async function BooksFech() {
-  const axios = localAxios();
   const getBook = async () => {
     console.log("fetching book......", Date.now());
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-    const response = await axios.get("/book");
+    const response = await axios.get("/book/979-11-57");
     // throw new Error("Someting wrong!!!!");
     console.log(response.data);
     return response.data;
